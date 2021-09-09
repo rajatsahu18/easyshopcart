b828b79742de4565f2ee478c3bc70f601ee02f73
Rajat Sahu <sahurajat78@gmail.com>
2021-09-09 18:35:19 +0530

    products

--- easy-shop-cart/src/Components/AllProducts/Products.jsx
+++ easy-shop-cart/src/Components/AllProducts/Products.jsx
@@ -34,9 +34,6 @@ const Products = () => {
                     </Link>
                 ))}
             </div>
-            <Electronics/>
-            <Clothes/>
-            <Accessories/>
         </>
     )
 }
